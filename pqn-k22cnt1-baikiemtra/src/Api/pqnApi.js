763b84e38263fc0b193d40a6849c3474ee64599f
Phạm Quang Nhất <164979267+phamquangnhatcntt1@users.noreply.github.com>
2024-07-08 12:48:20 +0700

    Update pqnApi.js

--- pqn-k22cnt1-baikiemtra/src/Api/pqnApi.js
+++ pqn-k22cnt1-baikiemtra/src/Api/pqnApi.js
@@ -1,21 +1,4 @@
-import axios from 'axios';
-
-axios.get('https://668b4f3f0b61b8d23b0914fd.mockapi.io/pqnApi/v1/')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    if (error.response) {
-      // Request made and server responded with a status code
-      console.log(error.response.data); // lỗi từ server
-      console.log(error.response.status); // mã lỗi từ server
-      console.log(error.response.headers); // headers từ server
-    } else if (error.request) {
-      // Yêu cầu được gửi nhưng không có phản hồi
-      console.log(error.request);
-    } else {
-      // Xảy ra lỗi khi thiết lập yêu cầu
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-  });
+import axios from 'axios'
+export default  axios.create({
+    baseURL: "https://668b4f3f0b61b8d23b0914fd.mockapi.io/pqnApi/v1/"
+})
