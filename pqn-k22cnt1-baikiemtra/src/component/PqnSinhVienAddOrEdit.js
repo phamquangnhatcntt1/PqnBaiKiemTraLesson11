17b71f1f7510a577985f254c3201287201dde6c9
Phạm Quang Nhất <164979267+phamquangnhatcntt1@users.noreply.github.com>
2024-07-08 12:47:48 +0700

    Update PqnSinhVienAddOrEdit.js

--- pqn-k22cnt1-baikiemtra/src/component/PqnSinhVienAddOrEdit.js
+++ pqn-k22cnt1-baikiemtra/src/component/PqnSinhVienAddOrEdit.js
@@ -1,208 +1,108 @@
+import React, { useEffect, useState } from 'react'
+import axios from '../Api/pqnApi';
 
-import axios from "../Api/pqnApi.js";
-import React, { useEffect, useState } from "react";
 
-export default function PqnSinhVienAddOrEdi({
-  onPqnClose,
-  onPqnSubmitForm,
-  renderUsers,
-}) {
-  console.log(renderUsers);
-  const [PqnMaSV, setPqnMaSV] = useState(0);
-  const [PqnHoSV, setPqnHoSV] = useState("");
-  const [PqnTenSV, setPqnTenSV] = useState("");
-  const [PqnPhai, setPqnPhai] = useState("");
-  const [PqnNgaySinh, setPqnNgaySinh] = useState("");
-  const [PqnNoiSinh, setPqnNoiSinh] = useState("");
-  const [PqnMaKH, setPqnMaKH] = useState("");
-  const [PqnHocBong, setPqnHocBong] = useState("");
-  const [PqnDiemTrungBinh, setPqnDiemTrungBinh] = useState("");
+export default function PqnSinhVienAddOrEdit(onPqnClose, onPqnSubmitForm, renderStudent) {
+    console.log(renderStudent);
+    const [pqnMaSV, setPqnMaSV] = useState(0);
+    const [pqnHoSV, setPqnHoSV] = useState("");
+    const [pqnTenSV, setPqnTenSV] = useState("");
+    const [pqnPhai, setPqnPhai] = useState("");
+    const [pqnNgaySinh, setPqnNgaySinh] = useState("");
+    const [pqnNoiSinh, setPqnNoiSinh] = useState("");
+    const [pqnMaKH, setPqnMaKH] = useState("");
+    const [pqnHocBong, setPqnHocBong] = useState("");
+    const [pqnDiemTrungBinh, setPqnDiemTrungBinh] = useState("");
 
-  useEffect(() => {
-    setPqnMaSV(renderUsers.PqnMaSV);
-    setPqnHoSV(renderUsers.PqnHoSV);
-    setPqnTenSV(renderUsers.PqnTenSV);
-    setPqnPhai(renderUsers.PqnPhai);
-    setPqnNgaySinh(renderUsers.PqnNgaySinh);
-    setPqnNoiSinh(renderUsers.PqnNoiSinh);
-    setPqnMaKH(renderUsers.PqnMaKH);
-    setPqnHocBong(renderUsers.PqnHocBong);
-    setPqnDiemTrungBinh(renderUsers.PqnDiemTrungBinh);
-  }, [renderUsers]);
+    useEffect(() => {
+        if (renderStudent) {
+            setPqnMaSV(renderStudent.MaSV);
+            setPqnHoSV(renderStudent.HoSV);
+            setPqnTenSV(renderStudent.TenSV);
+            setPqnPhai(renderStudent.Phai);
+            setPqnNgaySinh(renderStudent.NgaySinh);
+            setPqnNoiSinh(renderStudent.NgaySinh);
+            setPqnMaKH(renderStudent.NgaySinh);
+            setPqnHocBong(renderStudent.HocBong);
+            setPqnDiemTrungBinh(renderStudent.DiemTrungBinh);
+        }
+    }, [renderStudent]);
+    const pqnHandleClose = () => {
+        onPqnClose(false);
+    }
 
-  const pqnHandleClose = () => {
-    onPqnClose(false);
-  };
+    const pqnHandleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(pqnMaSV, pqnHoSV, pqnTenSV, pqnPhai, pqnNgaySinh, pqnNoiSinh, pqnMaKH, pqnHocBong, pqnDiemTrungBinh);
+        let pqnObjectStudent = {
+            PqnMaSV: pqnMaSV,
+            PqnHoSV: pqnHoSV,
+            PqnTenSV: pqnTenSV,
+            PqnPhai: pqnPhai,
+            PqnNgaySinh: pqnNgaySinh,
+            PqnNoiSinh: pqnNoiSinh,
+            PqnMaKH: pqnMaKH,
+            PqnHocBong: pqnHocBong,
+            PqnDiemTrungBinh: pqnDiemTrungBinh
+        }
+        const pqnRes = await axios.post("PqnSinhVien", pqnObjectStudent);
+        onPqnSubmitForm(false);
+    }
+    return (
+        <div className='border'>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="MaSV">MaSV</span>
+                <input type="text" class="form-control"
+                    name='MaSV' value={pqnMaSV} onChange={(ev) => setPqnMaSV(ev.target.value)}
+                    placeholder="MaSV" aria-label="MaSV" aria-describedby="MaSV" />
+            </div>
 
-  const pqnHandleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(PqnMaSV, PqnHoSV, PqnTenSV, PqnPhai, PqnNgaySinh,PqnNoiSinh,PqnMaKH,PqnHocBong,PqnDiemTrungBinh);
-    // post -> api
-    let pqnObjSinhVien = {
-        PqnHoSV: PqnHoSV,
-        PqnTenSV: PqnTenSV,
-        PqnPhai: PqnPhai,
-        PqnNgaySinh: PqnNgaySinh,
-        PqnNoiSinh: PqnNoiSinh,
-        PqnMaKH: PqnMaKH,
-        PqnHocBong: PqnHocBong,
-        PqnDiemTrungBinh: PqnDiemTrungBinh,
-        PqnMaSV: PqnMaSV,
-    };
-    const pqnRes = await axios.post("PqnSinhVien", pqnObjSinhVien);
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="HoSV">HoSV</span>
+                <input type="text" class="form-control"
+                    name='HoSV' value={pqnHoSV} onChange={(ev) => setPqnHoSV(ev.target.value)}
+                    placeholder="HoSV" aria-label="HoSV" aria-describedby="HoSV" />
+            </div>
 
-    onPqnSubmitForm(false);
-  };
-  return (
-    <div className="">
-      <form>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="MaSV">
-            MaSV
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            name="MaSV"
-            value={PqnMaSV}
-            onChange={(ev) => setPqnMaSV(ev.target.value)}
-            placeholder="MaSV"
-            aria-label="MaSV"
-            aria-describedby="MaSV"
-          />
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="TenSV">TenSV</span>
+                <input type="text" class="form-control"
+                    name='TenSV' value={pqnTenSV} onChange={(ev) => setPqnTenSV(ev.target.value)}
+                    placeholder="TenSV" aria-label="TenSV" aria-describedby="TenSV" />
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="Phai">Phai</span>
+                <input type="text" class="form-control"
+                    name='Phai' value={pqnPhai} onChange={(ev) => setPqnPhai(ev.target.value)}
+                    placeholder="Phai" aria-label="Phai" aria-describedby="Phai" />
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="NoiSinh">NoiSinh</span>
+                <input type="text" class="form-control"
+                    name='NoiSinh' value={pqnNoiSinh} onChange={(ev) => setPqnNoiSinh(ev.target.value)}
+                    placeholder="NoiSinh" aria-label="NoiSinh" aria-describedby="NoiSinh" />
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="MaKH">MaKH</span>
+                <input type="text" class="form-control"
+                    name='MaKH' value={pqnMaKH} onChange={(ev) => setPqnMaKH(ev.target.value)}
+                    placeholder="MaKH" aria-label="MaKH" aria-describedby="MaKH" />
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="HocBong">HocBong</span>
+                <input type="text" class="form-control"
+                    name='HocBong' value={pqnHocBong} onChange={(ev) => setPqnHocBong(ev.target.value)}
+                    placeholder="HocBong" aria-label="HocBong" aria-describedby="HocBong" />
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="DiemTrungBinh">DiemTrungBinh</span>
+                <input type="text" class="form-control"
+                    name='DiemTrungBinh' value={pqnDiemTrungBinh} onChange={(ev) => setPqnDiemTrungBinh(ev.target.value)}
+                    placeholder="DiemTrungBinh" aria-label="DiemTrungBinh" aria-describedby="DiemTrungBinh" />
+            </div>
+            
+            <button className='btn btn-primary' name='btnPqnSave' onClick={(ev) => pqnHandleSubmit(ev)}>Ghi lai</button>
+            <button className='btn btn-danger' onClick={pqnHandleClose}>Dong</button>
         </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="HoSV">
-            HoSV
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            name="HoSV"
-            value={PqnHoSV}
-            onChange={(ev) => setPqnHoSV(ev.target.value)}
-            placeholder="HoSV"
-            aria-label="HoSV"
-            aria-describedby="HoSV"
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="TenSV">
-            TenSV
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            name="TenSV"
-            value={PqnTenSV}
-            onChange={(ev) => setPqnTenSV(ev.target.value)}
-            placeholder="TenSV"
-            aria-label="TenSV"
-            aria-describedby="TenSV"
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="Phai">
-            Phai
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            name="Phai"
-            value={PqnPhai}
-            onChange={(ev) => setPqnPhai(ev.target.value)}
-            placeholder="Phai"
-            aria-label="Phai"
-            aria-describedby="Phai"
-          />
-        </div>
-
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="NgaySinh">
-            NgaySinh
-          </span>
-          <input
-            type="date"
-            class="form-control"
-            name="NgaySinh"
-            value={PqnNgaySinh}
-            onChange={(ev) => setPqnNgaySinh(ev.target.value)}
-            placeholder="NgaySinh"
-            aria-label="NgaySinh"
-            aria-describedby="NgaySinh"
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="NoiSinh">
-            NgaySinh
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            name="NoiSinh"
-            value={PqnNgaySinh}
-            onChange={(ev) => setPqnNoiSinh(ev.target.value)}
-            placeholder="NoiSinh"
-            aria-label="NoiSinh"
-            aria-describedby="NoiSinh"
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="MaKH">
-            NgaySinh
-          </span>
-          <input
-            type="number"
-            class="form-control"
-            name="PqnMaKH"
-            value={PqnNgaySinh}
-            onChange={(ev) => setPqnMaKH(ev.target.value)}
-            placeholder="MaKH"
-            aria-label="MaKH"
-            aria-describedby="MaKH"
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="HocBong">
-            NgaySinh
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            name="HocBong"
-            value={PqnNgaySinh}
-            onChange={(ev) => setPqnHocBong(ev.target.value)}
-            placeholder="HocBong"
-            aria-label="HocBong"
-            aria-describedby="HocBong"
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text" id="DiemTrungBinh">
-            NgaySinh
-          </span>
-          <input
-            type="number"
-            class="form-control"
-            name="DiemTrungBinh"
-            value={PqnNgaySinh}
-            onChange={(ev) => setPqnDiemTrungBinh(ev.target.value)}
-            placeholder="DiemTrungBinh"
-            aria-label="DiemTrungBinh"
-            aria-describedby="DiemTrungBinh"
-          />
-        </div>
-        <button
-          className="btn btn-primary"
-          name="btnPqnSave"
-          onClick={(ev) => pqnHandleSubmit(ev)}
-        >
-          Ghi lại
-        </button>
-        <button className="btn btn-danger" onClick={pqnHandleClose}>
-          Đóng
-        </button>
-      </form>
-    </div>
-  );
+    )
 }
